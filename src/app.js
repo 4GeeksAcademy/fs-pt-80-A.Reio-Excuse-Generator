769fcc72c840991f763addc6833d3098e3e3e83b
AJReio <alejandrorello@gmail.com>
2024-10-06 18:36:18 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const quien = [
+  "Mi profesor ",
+  "Mi hermano ",
+  "El párroco ",
+  "Una multitud enfurecida "
+];
+const hizo = [
+  "arrancó de mis manos ",
+  "lanzó por los aires ",
+  "vendió por wallapop ",
+  "cambió por unos tazos "
+];
+const aQue = [
+  "la dentadura de mi abuela ",
+  "los dos últimos chicles ",
+  "mis medicinas ",
+  "mi nintendo switch "
+];
+const cuando = [
+  "mientras ayudaba a un ciego ",
+  "mientras yo rezaba ",
+  "durante mi segunda siesta ",
+  "cuando estaba despistado "
+];
+
+const opciones = array => Math.floor(Math.random() * array.length);
+const miExcusa = array => array[opciones(array)];
+
+console.log(miExcusa(quien));
+console.log(miExcusa(hizo));
+console.log(miExcusa(aQue));
+console.log(miExcusa(cuando));
